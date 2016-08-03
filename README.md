87e749f6fb7243c7b9d8593a2158b9f0d644cba0
cornelk <github@codejuggle.dj>
2016-08-03 02:28:17 +0200

    Benchmark added

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # hashmap
-A Golang  thread-safe HashMap optimized for lock-free read access
+A Golang thread-safe HashMap optimized for lock-free read access.
+
+Reading from the hash map in a thread-safe way is faster than reading from standard go map in an unsafe way:
+
+```
+go test -bench=.
+
+BenchmarkReadHashMap-4            300000              5503 ns/op
+BenchmarkReadGoMap-4              200000              6609 ns/op
+BenchmarkReadGoMapSafe-4           30000             42261 ns/op
+```
---
+++ benchmark_test.go
@@ -0,0 +1,72 @@
+package hashmap
+
+import (
+	"sync"
+	"testing"
+)
+
+func setupHashMap(b *testing.B) *HashMap {
+	b.StopTimer()
+	m := New()
+	for i := 0; i < 1024; i++ {
+		m.Set(uint64(i), uint64(i), uint64(i))
+	}
+	b.StartTimer()
+	return m
+}
+
+func setupGoMap(b *testing.B) map[uint64]uint64 {
+	b.StopTimer()
+	m := make(map[uint64]uint64)
+	for i := 0; i < 1024; i++ {
+		m[uint64(i)] = uint64(i)
+	}
+	b.StartTimer()
+	return m
+}
+
+func BenchmarkReadHashMap(b *testing.B) {
+	m := setupHashMap(b)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i := 0; i < 1024; i++ {
+				j, _ := m.Get(uint64(i), uint64(i))
+				if j.(uint64) != uint64(i) {
+					b.Fail()
+				}
+			}
+		}
+	})
+}
+
+func BenchmarkReadGoMap(b *testing.B) {
+	m := setupGoMap(b)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i := 0; i < 1024; i++ {
+				j, _ := m[uint64(i)]
+				if j != uint64(i) {
+					b.Fail()
+				}
+			}
+		}
+	})
+}
+
+func BenchmarkReadGoMapSafe(b *testing.B) {
+	m := setupGoMap(b)
+	l := &sync.RWMutex{}
+	b.StartTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i := 0; i < 1024; i++ {
+				l.RLock()
+				j, _ := m[uint64(i)]
+				l.RUnlock()
+				if j != uint64(i) {
+					b.Fail()
+				}
+			}
+		}
+	})
+}
