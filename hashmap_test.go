4e35cd2348bd924e2ce94d390fd04ffde09d9144
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2018-06-19 12:05:30 +0200

    parallel hashmap test

--- hashmap_test.go
+++ hashmap_test.go
@@ -383,3 +383,106 @@ func TestExample(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestHashMap_parallel(t *testing.T) {
+	max := 10
+	dur := 2 * time.Second
+	m := &HashMap{}
+
+	do := func(t *testing.T, max int, d time.Duration, fn func(*testing.T, int)) <-chan error {
+		t.Helper()
+
+		done := make(chan error)
+
+		var times int64
+
+		// This goroutines will terminate test in case if closure hangs.
+		go func() {
+			for {
+				select {
+				case <-time.After(d + 500*time.Millisecond):
+					if atomic.LoadInt64(&times) == 0 {
+						done <- fmt.Errorf("closure was not executed even once, something blocks it")
+					}
+					close(done)
+				case <-done:
+				}
+			}
+		}()
+
+		go func() {
+			timer := time.NewTimer(d)
+			defer timer.Stop()
+
+		InfLoop:
+			for {
+				for i := 0; i < max; i++ {
+					select {
+					case <-timer.C:
+						break InfLoop
+					default:
+					}
+
+					fn(t, i)
+					atomic.AddInt64(&times, 1)
+				}
+			}
+
+			close(done)
+		}()
+
+		return done
+	}
+	wait := func(t *testing.T, done <-chan error) {
+		t.Helper()
+
+		if err := <-done; err != nil {
+			t.Error(err)
+		}
+	}
+
+	// Initial fill.
+	for i := 0; i < max; i++ {
+		m.Set(i, i)
+		m.Set(fmt.Sprintf("%d", i), i)
+		m.SetHashedKey(uintptr(i), i)
+	}
+
+	t.Run("set_get", func(t *testing.T) {
+		doneSet := do(t, max, dur, func(t *testing.T, i int) {
+			m.Set(i, i)
+		})
+		doneGet := do(t, max, dur, func(t *testing.T, i int) {
+			if _, ok := m.Get(i); !ok {
+				t.Errorf("missing value for key: %d", i)
+			}
+		})
+		doneGetStringKey := do(t, max, dur, func(t *testing.T, i int) {
+			if _, ok := m.GetStringKey(fmt.Sprintf("%d", i)); !ok {
+				t.Errorf("missing value for key: %d", i)
+			}
+		})
+		doneGetHashedKey := do(t, max, dur, func(t *testing.T, i int) {
+			if _, ok := m.GetHashedKey(uintptr(i)); !ok {
+				t.Errorf("missing value for key: %d", i)
+			}
+		})
+
+		wait(t, doneSet)
+		wait(t, doneGet)
+		wait(t, doneGetStringKey)
+		wait(t, doneGetHashedKey)
+	})
+
+	t.Run("get-or-insert-and-delete", func(t *testing.T) {
+		doneGetOrInsert := do(t, max, dur, func(t *testing.T, i int) {
+			m.GetOrInsert(i, i)
+		})
+		doneDel := do(t, max, dur, func(t *testing.T, i int) {
+			m.Del(i)
+		})
+
+		wait(t, doneGetOrInsert)
+		wait(t, doneDel)
+	})
+}
