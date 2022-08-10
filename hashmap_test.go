836bcfbdf7769a0c95ed7f5bd015fb84e95d81b6
cornelk <github@codejuggle.dj>
2022-08-09 22:41:06 -0600

    add test for a bug

--- hashmap_test.go
+++ hashmap_test.go
@@ -3,6 +3,7 @@ package hashmap
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"strconv"
 	"sync"
 	"sync/atomic"
@@ -402,4 +403,58 @@ func TestHashMap_SetConcurrent(t *testing.T) {
 	}
 
 	wg.Wait()
+}
+
+func TestConcurrentInsertDelete(t *testing.T) {
+	for i := 0; i < 200; i++ {
+		el1 := &ListElement[int, int]{
+			key:     111,
+			keyHash: 111,
+		}
+		el2 := &ListElement[int, int]{
+			key:     222,
+			keyHash: 222,
+		}
+		el3 := &ListElement[int, int]{
+			key:     333,
+			keyHash: 333,
+		}
+		newIl := &ListElement[int, int]{
+			key:     223,
+			keyHash: 223,
+		}
+		l := NewList[int, int]()
+		l.Add(el1, nil)
+		l.Add(el2, nil)
+		l.Add(el3, nil)
+		wg := sync.WaitGroup{}
+		wg.Add(2)
+
+		go func() {
+			rand.Seed(int64(time.Now().Nanosecond()))
+			time.Sleep(time.Duration(rand.Intn(10)))
+			l.Delete(el2)
+			wg.Done()
+		}()
+		go func() {
+			defer wg.Done()
+			rand.Seed(int64(time.Now().Nanosecond()))
+			time.Sleep(time.Duration(rand.Intn(10)))
+			for {
+				if _, inserted := l.Add(newIl, nil); inserted {
+					return
+				}
+			}
+		}()
+		wg.Wait()
+
+		if le := l.Len(); le != 3 {
+			t.Error("l.Len() != 3", le)
+			return
+		}
+		if _, found, _ := l.search(nil, newIl); found == nil {
+			t.Error("newIl not found")
+			return
+		}
+	}
 }
